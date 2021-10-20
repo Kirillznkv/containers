5cb3b47215a5b434e7c3eda10ed58166304eb3d6
Kenda Shanti <kshanti@ev-f5.kzn.21-school.ru>
2021-10-20 23:59:25 +0300

    finish container for stack

--- container.hpp
+++ container.hpp
@@ -1,27 +1,109 @@
 #ifndef CONTAINER_HPP
 #define CONTAINER_HPP
 
-namespace ft {
-
-class Vector;
-class Stack;
-class Map;
+namespace ft{
 
 template<typename T>
-class Container {
+struct containerData{
+	T value;
+	containerData *next;
+};
+
+template<typename T>
+class Container{
 private:
-	friend Vector;
-	friend Stack;
-	friend Map;
-private:
-	T *array;
-	unsigned int size;
+	containerData<T> *_data;
+	size_t _size;
+	containerData<T> *newElem(const T &newValue){
+		containerData<T> *elem = new containerData<T>;
+		elem->value = newValue;
+		elem->next = NULL;
+		return (elem);
+	}
 public:
-	Container(void);
-	~Container(void);
-	Container(const Container &copy);
-	Container &operator=(const Container &op);
-	void push_back(T &va)
+	Container(void) : _size(0), _data(NULL){}
+	~Container(void){
+		containerData<T> *freeData;
+		while (_data){
+			freeData = _data;
+			_data = _data->next;
+			delete freeData;
+		}
+	}
+	Container(const Container &copy){
+		this->operator=(copy);
+	}
+	Container &operator=(const Container &op){
+		if (this == &op)
+			return (*this);
+		this->_size = op.size();
+		containerData<T> *elem = op._data;
+		for (int i = 0; i < (int)this->_size; ++i){
+			this->push_back(elem->value);
+			elem = elem->next;
+		}
+		return (*this);
+	}
+	void push_back(const T &newValue){
+		if (!_data)
+			_data = newElem(newValue);
+		else{
+			containerData<T> *i = _data;
+			while (i->next)
+				i = i->next;
+			i->next = newElem(newValue);
+		}
+		_size++;
+	}
+	void push_front(const T &newValue){
+		containerData<T> *oldBegin = _data;
+		_data = newElem(newValue);
+		_data->next = oldBegin;
+		_size++;
+	}
+	void pop_back(void){
+		if (!_data)
+			return ;
+		if (!(_data->next)){
+			delete _data;
+			_data = NULL;
+			return ;
+		}
+		containerData<T> *i = _data;
+		while (i->next->next)
+			i = i->next;
+		delete i->next;
+		i->next = NULL;
+		_size--;
+	}
+	void pop_front(void){
+		if (!_data)
+			return ;
+		if (!(_data->next)){
+			delete _data;
+			_data = NULL;
+			return ;
+		}
+		containerData<T> *newBegin = _data->next;
+		delete _data;
+		_data = newBegin;
+		_size--;
+	}
+	T &begin(void) const{
+		return (_data->value);
+	}
+	T &end(void) const{
+		containerData<T> *i = _data;
+		while (i->next)
+			i = i->next;
+		return (i->value);
+	}
+	bool isEmpty(void) const{
+		return (_data?false:true);
+	}
+	size_t size(void) const{
+		return (_size);
+	}
 };
 
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 #include <stack>
 #include <vector>
 
+#include "container.hpp"
+
 int	main() {
+	ft::Container<int> s;
+	std::cout<<s.size()<<std::endl;
+	s.push_back(1);
+	s.push_back(2);
+	s.push_back(3);
+	s.push_front(4);
+	std::cout<<s.begin()<<std::endl;
+	std::cout<<s.end()<<std::endl;
+	s.pop_front();
+	std::cout<<s.size()<<std::endl;
+	std::cout<<s.begin()<<std::endl;
+	std::cout<<s.end()<<std::endl;
 	return (0);
 }
