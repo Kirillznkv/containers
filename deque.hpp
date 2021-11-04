fe03343c0f0c52d1b0a0608fffe0ae08bb954398
kshanti_home <Zenich99@gmail.com>
2021-11-05 00:46:28 +0300

    add typedef (md finish stack)

--- deque.hpp
+++ deque.hpp
@@ -11,13 +11,13 @@ public:
 	typedef const T&	const_reference;
 	typedef size_t		size_type;
 
-	// typedef Allocator									allocator_type;
-	// typedef typename allocator_type::reference			reference;
-	// typedef typename allocator_type::const_reference	const_reference;
-	// typedef typename allocator_type::size_type			size_type;
-	// typedef typename allocator_type::difference_type	difference_type;
-	// typedef typename allocator_type::pointer			pointer;
-	// typedef typename allocator_type::const_pointer		const_pointer;
+	typedef Allocator									allocator_type;
+	typedef typename allocator_type::reference			reference_alloc;
+	typedef typename allocator_type::const_reference	const_reference_alloc;
+	typedef typename allocator_type::size_type			size_type_alloc;
+	typedef typename allocator_type::difference_type	difference_type_alloc;
+	typedef typename allocator_type::pointer			pointer_alloc;
+	typedef typename allocator_type::const_pointer		const_pointer_alloc;
 private:
 	struct containerData{
 		value_type *value;
@@ -26,12 +26,12 @@ private:
 	containerData *_data;
 	size_type _size;
 	std::allocator<containerData > _allocatorNode;
-	std::allocator<value_type > _allocatorData;
+	allocator_type _allocatorData;
+
 	containerData *newElem(const value_type& newValue){
 		containerData *elem = _allocatorNode.allocate(1);
 		elem->value = _allocatorData.allocate(1);
 		_allocatorData.construct(elem->value, newValue);
-		// *(elem->value) = newValue;
 		value_type a = newValue;
 		a = (value_type)a;
 		elem->next = NULL;
